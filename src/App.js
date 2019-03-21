7392896449425f98a9bb20c0ce37ec5557beec05
dvsilva <Danyllo Silva>
2019-03-21 10:03:02 -0300

    finishing project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, { Fragment } from 'react';
-import GlobalStyle from './styles/global';
+import React, { Fragment } from "react";
+
+import GlobalStyle from "./styles/global";
+
+import Main from "./pages/Main";
 
 /**
 const Title = styled.h1`
   color: '#F00';
   font-size: 32px;
 `;
- */
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
     <div className="App" />
+  </Fragment>
+);
+ */
+
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Main />
   </Fragment>
 );
 
